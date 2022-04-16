73757a844a149fa5f3c94abb97df244bdbc7c628
Tino <73894557+tinofay@users.noreply.github.com>
2022-04-16 16:00:58 +0200

    Update RandomRectangleGUI.java

--- RandomRectangleGUI.java
+++ RandomRectangleGUI.java
@@ -1,25 +1,23 @@
+package com.assignment.tino
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-/**
- * Created by Tinotenda Zungunde on 24/10/2021.
- */
-
 public class RandomRectangleGUI{
-	JFrame frame;
-	RandomRectDrawPanel drawPanel =new RandomRectDrawPanel();
-	JButton colorButton;
-	JButton sizeButton;
-
+	private JFrame frame;
+	private JButton colorButton;
+	private JButton sizeButton;
+	
+	private final RandomRectDrawPanel rectDrawPanel = new RandomRectDrawPanel();
+	
 	public static void main (String[] args){
 		RandomRectangleGUI gui = new RandomRectangleGUI();
-		gui.go();
+		gui.createJFrame();
 	}
 
-	public void go(){
-		frame = new JFrame();
+	public void createJFrame(){
+		frame = new JFrame("TinosPlayFrame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		colorButton = new JButton("Click me for a random colour");
@@ -38,23 +36,24 @@ public class RandomRectangleGUI{
 
 
 	class RandomRectDrawPanel extends JPanel{
-		Color color;
-		int height = 50;
-		int width = 80;
-		int x = 50;
-		int y = 50;
+		
+		private Color backgroundColor;
+		private int height = 50;
+		private int width = 80;
+		private int xCartesianPosition = 50;
+		private int yCartesianPosition = 50;
 
 		public void paintComponent (Graphics g){
 			super.paintComponent(g);
-			g.setColor(color);
-			g.fillRect(x,y,width,height);
+			g.setColor(backgroundColor);
+			g.fillRect(xCartesianPosition,yCartesianPosition,width,height);
 		}
 		
 		public void randomColor(){
 			int r = (int)(Math.random()*255);
 			int gr = (int)(Math.random()*255);
 			int b = (int)(Math.random()*255);
-			color = new Color(r,gr,b);
+			backgroundColor = new Color(r,gr,b);
 		}
 		
 		public void randomSize(){
@@ -63,14 +62,14 @@ public class RandomRectangleGUI{
 			width = (int)(Math.random()*getWidth());
 
 			int temp;
-			if ((y + height) > getHeight()){  // this to keep all of the height of the rectangle inside the draw panel
-				temp = getHeight() - (y + height);
+			if ((yCartesianPosition + height) > getHeight()){  // this to keep all of the height of the rectangle inside the draw panel
+				temp = getHeight() - (yCartesianPosition + height);
 				height = height + temp - displace;  // temp is a negative number
 			}
 			if (height < 5) height = 5;//minimum height
 
-			if ((x + width) > getWidth()){  // this to keep all of the width of the rectangle inside the draw panel
-				temp = getWidth() - (x + width);
+			if ((xCartesianPosition + width) > getWidth()){  // this to keep all of the width of the rectangle inside the draw panel
+				temp = getWidth() - (xCartesianPosition + width);
 				width = width + temp - displace;  // temp is a negative number
 			}
 			if (width < 5) width = 5; //minimum width
@@ -80,16 +79,16 @@ public class RandomRectangleGUI{
 	class RandomColorListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			drawPanel.randomColor();
-			drawPanel.repaint();
+			rectDrawPanel.randomColor();
+			rectDrawPanel.repaint();
 		}
 	}
 
 	class SizeListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			drawPanel.randomSize();
-			drawPanel.repaint();
+			rectDrawPanel.randomSize();
+			rectDrawPanel.repaint();
 		}
 	}
 
